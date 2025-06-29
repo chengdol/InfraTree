e4de5ff44fb2275a221f427f77d37f55a720391c
chengdol <hhgjlcd@gmail.com>
2025-06-28 23:43:37 -0700

    vagrant mysql community 8.0.42

---
+++ vagrant-mysql-standalone/provision/provision-mysql-8.0.42.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This is the provisioning script to install MySQL community service version
+# 8.0.42 on rocky Linux arm64.
+
+MYSQL_VERSION='8.0.42'
+DOWNLOAD_PATH="https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-${MYSQL_VERSION}-1.el9.aarch64.rpm-bundle.tar"
+
+LogMsg()
+{
+  # parse input and reformat
+  logMsg="$@"
+  echo "["`date +"%Y/%m/%d %r"`"] " ${logMsg}
+}
+
+LogMsg "Download Mysql community server version ${MYSQL_VERSION}..."
+wget -q ${DOWNLOAD_PATH}
+
+LogMsg 'uncompress download tar file...'
+tar -xvf mysql-${MYSQL_VERSION}-1.el9.aarch64.rpm-bundle.tar
+
+LogMsg 'Install essential rpms...'
+sudo dnf install -q -y \
+  mysql-community-common-${MYSQL_VERSION}-1.el9.aarch64.rpm \
+  mysql-community-libs-${MYSQL_VERSION}-1.el9.aarch64.rpm \
+  mysql-community-client-plugins-${MYSQL_VERSION}-1.el9.aarch64.rpm \
+  mysql-community-client-${MYSQL_VERSION}-1.el9.aarch64.rpm \
+  mysql-community-server-${MYSQL_VERSION}-1.el9.aarch64.rpm \
+  mysql-community-icu-data-files-${MYSQL_VERSION}-1.el9.aarch64.rpm
+
+LogMsg 'Enable mysqld...'
+sudo systemctl enable mysqld
+LogMsg 'Start mysqld...'
+sudo systemctl start mysqld
+
+LogMsg "Mysql server community version ${MYSQL_VERSION} is up and running!"
